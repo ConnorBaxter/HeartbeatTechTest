131944e6697da976d180e94f2f1d60072e7bc1fa
Connor Baxter <connor.bax@protonmail.com>
2024-05-24 22:22:31 +0100

    Api now logs message to logger

---
+++ Heartbeat.Api/Controllers/StatusController.cs
@@ -0,0 +1,25 @@
+﻿namespace Heartbeat.Api.Controllers
+{
+    using Heartbeat.Lib;
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostHeartbeat([FromBody] StatusResource statusResource)
+        {
+            this.logger.LogInformation($"Received heartbeat: {JsonConvert.SerializeObject(statusResource)}");
+            return new OkResult();
+        }
+    }
+}
--- Heartbeat.Service/MessageSenders/MessageSender.cs
+++ Heartbeat.Service/MessageSenders/MessageSender.cs
@@ -1,15 +1,25 @@
 ﻿namespace Heartbeat.Service.MessageSenders
 {
+    using System.Net.Http.Json;
     using Heartbeat.Lib;
+    using Heartbeat.Service.Configuration;
 
     public class MessageSender : IMessageSender
     {
-        private readonly HttpClient Client = new HttpClient();
+        private readonly HttpClient Client = new();
         
+        private readonly HeartbeatServiceSettings settings;
+
+        public MessageSender(HeartbeatServiceSettings settings)
+        {
+            this.settings = settings;
+        }
+
         public async Task<bool> Send(StatusResource statusResource)
         {
-            //this.Client.PostAsJsonAsync()
-            return true;
+            var endpoint = this.settings.Url + this.settings.StatusEndpoint;
+            var response = await this.Client.PostAsJsonAsync(endpoint, statusResource);
+            return response.IsSuccessStatusCode;
         }
     }
 }
